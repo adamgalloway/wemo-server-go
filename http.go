bb3c2f68841b0c57bfb58a2d9206522fb22110b8
adamgalloway <adam.galloway@gmail.com>
2019-07-04 16:58:00 +0000

    adds upnp event handler

--- http.go
+++ http.go
@@ -2,6 +2,7 @@ package main
  
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
     "strings"
     "strconv"
@@ -48,14 +49,43 @@ func setupResponse(name string, id string, serial string) string {
     return res.String()
 }
 
-func setupHandler(w http.ResponseWriter, r *http.Request) {
-    res := setupResponse("testswitch", "aa993f4a-375f-4cf6-98d7-17bfc0f2290d", "000002F0101C00")
-    w.Header().Set("Content-Type", "text/xml")
-    fmt.Fprintf(w, res)
+func setupHandler(name string, id string, serial string) http.HandlerFunc {
+    res := setupResponse(name, id, serial)
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/xml")
+        fmt.Fprintf(w, res)
+    }
 }
 
-func upnpHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "upnp")
+func upnpResponse(state string) string {
+    var res strings.Builder
+    res.WriteString("<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">")
+    res.WriteString("<s:Body>")
+    res.WriteString("<u:SetBinaryStateResponse xmlns:u=\"urn:Belkin:service:basicevent:1\">")
+    res.WriteString("<BinaryState>")
+    res.WriteString(state)
+    res.WriteString("</BinaryState>")
+    res.WriteString("</u:SetBinaryStateResponse>")
+    res.WriteString("</s:Body>")
+    res.WriteString("</s:Envelope>\r\n\r\n")
+    return res.String()
+}
+
+func upnpHandler() http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        var state string
+        body, err := ioutil.ReadAll(r.Body)
+        if err == nil && strings.Contains(string(body), "<BinaryState>1</BinaryState>") {
+            state = "1"
+            // turn on
+        } else {
+            state = "0"
+            // turn off
+        }
+        res := upnpResponse(state)
+        w.Header().Set("Content-Type", "text/xml")
+        fmt.Fprintf(w, res)
+    }
 }
 
 
@@ -65,8 +95,8 @@ func eventHandler(w http.ResponseWriter, r *http.Request) {
 
 func HandleHttp(port int) {
     server := http.NewServeMux()
-    server.HandleFunc("/setup.xml", setupHandler)
-    server.HandleFunc("/upnp/control/basicevent1", upnpHandler)
+    server.HandleFunc("/setup.xml", setupHandler("testswitch", "aa993f4a-375f-4cf6-98d7-17bfc0f2290d", "000002F0101C00"))
+    server.HandleFunc("/upnp/control/basicevent1", upnpHandler())
     server.HandleFunc("/eventservice.xml", eventHandler)
     log.Fatal(http.ListenAndServe(":" + strconv.Itoa(port), server))
 }
