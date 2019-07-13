9423c539b5114200632f6f86b8ee977491235fa4
adamgalloway <adam.galloway@gmail.com>
2019-07-13 11:19:17 -0400

    Update http.go

--- http.go
+++ http.go
@@ -12,67 +12,57 @@ import (
     "text/template"
 )
 
-func setupResponse(name string, id string, serial string) string {
-    var res strings.Builder
-    res.WriteString("<?xml version=\"1.0\"?>")
-    res.WriteString("<root xmlns=\"urn:Belkin:device-1-0\">")
-        res.WriteString("<specVersion>")
-            res.WriteString("<major>1</major>")
-            res.WriteString("<minor>0</minor>")
-        res.WriteString("</specVersion>")
-        res.WriteString("<device>")
-             res.WriteString("<deviceType>urn:Belkin:device:controllee:1</deviceType>")
-             res.WriteString("<friendlyName>")
-             res.WriteString(name)
-             res.WriteString("</friendlyName>")
-             res.WriteString("<manufacturer>Belkin International Inc.</manufacturer>")
-             res.WriteString("<modelName>Emulated Socket</modelName>")
-             res.WriteString("<modelNumber>3.1415</modelNumber>")
-             res.WriteString("<manufacturerURL>http://www.belkin.com</manufacturerURL>")
-             res.WriteString("<modelDescription>Belkin Plugin Socket 1.0</modelDescription>")
-             res.WriteString("<modelURL>http://www.belkin.com/plugin/</modelURL>")
-             res.WriteString("<UDN>uuid:")
-             res.WriteString(id)
-             res.WriteString("</UDN>")
-             res.WriteString("<serialNumber>")
-             res.WriteString(serial)
-             res.WriteString("</serialNumber>")
-             res.WriteString("<binaryState>0</binaryState>")
-             res.WriteString("<serviceList>")
-                 res.WriteString("<service>")
-                     res.WriteString("<serviceType>urn:Belkin:service:basicevent:1</serviceType>")
-                     res.WriteString("<serviceId>urn:Belkin:serviceId:basicevent1</serviceId>")
-                     res.WriteString("<controlURL>/upnp/control/basicevent1</controlURL>")
-                     res.WriteString("<eventSubURL>/upnp/event/basicevent1</eventSubURL>")
-                     res.WriteString("<SCPDURL>/eventservice.xml</SCPDURL>")
-                 res.WriteString("</service>")
-             res.WriteString("</serviceList>")
-         res.WriteString("</device>")
-    res.WriteString("</root>\r\n\r\n")
-    return res.String()
-}
-
-func setupHandler(name string, id string, serial string) http.HandlerFunc {
-    res := setupResponse(name, id, serial)
-    return func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("setup request from", r.RemoteAddr)
-
-        w.Header().Set("Content-Type", "text/xml")
-        fmt.Fprintf(w, res)
-    }
-}
-
 var upnpResponse = template.New("upnpResponse")
+var setupResponse = template.New("setupResponse")
 
 func init() {
   upnpResponse.Parse(`<?xml version="1.0"?>
   <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <s:Body>
-      <u:{{method}}BinaryStateResponse xmlns:u="urn:Belkin:service:basicevent:1">
-        <BinaryState>{{state}}</BinaryState>
+      <u:{{.method}}BinaryStateResponse xmlns:u="urn:Belkin:service:basicevent:1">
+        <BinaryState>{{.state}}</BinaryState>
       </u:{{method}}BinaryStateResponse>
     </s:Body>
   </s:Envelope>\r\n`)
+
+  setupResponse.Parse(`<?xml version="1.0"?>
+  <root xmlns=\"urn:Belkin:device-1-0\">
+  <specVersion>
+    <major>1</major>
+    <minor>0</minor>
+  </specVersion>
+  <device>
+    <deviceType>urn:Belkin:device:controllee:1</deviceType>
+    <friendlyName>{{.name}}</friendlyName>
+    <manufacturer>Belkin International Inc.</manufacturer>
+    <modelName>Emulated Socket</modelName>
+    <modelNumber>3.1415</modelNumber>
+    <manufacturerURL>http://www.belkin.com</manufacturerURL>
+    <modelDescription>Belkin Plugin Socket 1.0</modelDescription>
+    <modelURL>http://www.belkin.com/plugin/</modelURL>
+    <UDN>uuid:{{.id}}</UDN>
+    <serialNumber>{{.serial}}</serialNumber>
+    <binaryState>0</binaryState>
+    <serviceList>
+      <service>
+        <serviceType>urn:Belkin:service:basicevent:1</serviceType>
+        <serviceId>urn:Belkin:serviceId:basicevent1</serviceId>
+        <controlURL>/upnp/control/basicevent1</controlURL>
+        <eventSubURL>/upnp/event/basicevent1</eventSubURL>
+        <SCPDURL>/eventservice.xml</SCPDURL>
+      </service>
+    </serviceList>
+  </device>
+  </root>\r\n`)
+}
+
+func setupHandler(name string, id string, serial string) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("setup request from", r.RemoteAddr)
+
+        w.Header().Set("Content-Type", "text/xml")
+        setupResponse.Execute(w, map[string]string{"name": name, "id": id, "serial": serial})
+    }
 }
 
 func upnpHandler(oncommand string, offcommand string) http.HandlerFunc {
