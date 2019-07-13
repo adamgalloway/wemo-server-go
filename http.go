e1263430575dbf5d7ec18335853cdb04de88dd3d
adamgalloway <adam.galloway@gmail.com>
2019-07-13 10:17:19 -0400

    Update http.go

--- http.go
+++ http.go
@@ -140,28 +140,40 @@ func upnpHandler(oncommand string, offcommand string) http.HandlerFunc {
 eventResponse := `<?xml version="1.0"?>
 <scpd xmlns="urn:Belkin:service-1-0">
 <actionList>
-<action>
-<name>SetBinaryState</name>
-<argumentList>
-<argument><retval/>
-<name>BinaryState</name>
-<relatedStateVariable>BinaryState</relatedStateVariable>
-<direction>in</direction>
-</argument>
-</argumentList>
-</action>
+  <action>
+    <name>SetBinaryState</name>
+    <argumentList>
+      <argument>
+        <retval/>
+        <name>BinaryState</name>
+        <relatedStateVariable>BinaryState</relatedStateVariable>
+        <direction>in</direction>
+      </argument>
+    </argumentList>
+  </action>
+  <action>
+    <name>GetBinaryState</name>
+    <argumentList>
+      <argument>
+        <retval/>
+        <name>BinaryState</name>
+        <relatedStateVariable>BinaryState</relatedStateVariable>
+        <direction>out</direction>
+      </argument>
+    </argumentList>
+  </action>
 </actionList>
 <serviceStateTable>
-<stateVariable sendEvents="yes">
-<name>BinaryState</name>
-<dataType>Boolean</dataType>
-<defaultValue>0</defaultValue>
-</stateVariable>
-<stateVariable sendEvents="yes">
-<name>level</name>
-<dataType>string</dataType>
-<defaultValue>0</defaultValue>
-</stateVariable>
+  <stateVariable sendEvents="yes">
+    <name>BinaryState</name>
+    <dataType>Boolean</dataType>
+    <defaultValue>0</defaultValue>
+  </stateVariable>
+  <stateVariable sendEvents="yes">
+    <name>level</name>
+    <dataType>string</dataType>
+    <defaultValue>0</defaultValue>
+  </stateVariable>
 </serviceStateTable>
 </scpd>\r\n`
 
